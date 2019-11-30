b85866d31ad00c1bf91027f299f8e162dd33e0af
Anton <ryb.anton.le@gmail.com>
2019-11-30 20:11:55 +0200

    some minor fix

--- js/custom-slider.js
+++ js/custom-slider.js
@@ -20,3 +20,37 @@ arrowLeft.forEach(function(item, index, arr){
        }
     });
 });
+
+
+let loadMoreBtn = document.querySelector('.load-more-btn');
+
+loadMoreBtn.addEventListener('click', function(e){
+  e.preventDefault();
+})
+
+loadMoreBtn.addEventListener('click',function(){
+    // console.log(galleryMore);
+    let galleryMore = document.querySelectorAll('.effect-apollo.no-active');
+    for(let i = 0; i < 9; i++){
+        galleryMore[i].classList.remove('no-active');
+    }
+    });
+
+
+
+
+    // let a_grid = document.querySelector('.grid');
+    // let a_gridChildren = a_grid.children;
+    // a_gridChildren = [...a_gridChildren];
+    
+
+// loadMoreBtn.addEventListener('click', addMoreImg);
+
+// function addMoreImg(){
+//     let figure = document.createElement('figure');
+//     figure.classList.add('col-md-4', 'effect-apollo');
+//     figure.innerHTML = `<img src="images/gallery/location-.jpg" alt="Odyssey"><figcaption><h4></h4></figcaption>`;
+//     // a_grid.appendChild(figure);
+//     console.log(figure);
+
+// }
